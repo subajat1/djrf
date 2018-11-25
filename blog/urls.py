d585dcea749a4d1f7448b6f8dbecdba11057ebf5
bayu.tech <bayu.labster@outlook.com>
2018-11-25 17:56:05 +0800

    develop post retrieve api

--- blog/urls.py
+++ blog/urls.py
@@ -31,6 +31,10 @@ urlpatterns = [
     url(r'^logout/', logout_view, name='logout'),
     url(r'^', include("posts.urls", namespace='posts')),
     #url(r'^posts/$', "<appname>.views.<function_name>"),
+    
+    # API urls
+    url(r'^api/posts/', include("posts.api.urls", namespace='posts-api')),
+    
 ]
 
 if settings.DEBUG:
